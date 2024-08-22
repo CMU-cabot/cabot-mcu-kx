4e33d758e86fb07e9db068b2980438027a41e36f
土屋 雄一 <59544467+yuichi-tsuchiya-kufusha@users.noreply.github.com>
2024-08-22 11:57:40 +0900

    add handle board mockup

---
+++ ais_stm32_handle_board_mock/ais_stm32_handle_board_mock.ino
@@ -0,0 +1,185 @@
+#include <STM32FreeRTOS.h>
+#include <mcp_can.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Dynamixel2Arduino.h>
+#include "Adafruit_VL53L0X.h"
+#include "cap1203.h" 
+
+#define SW_RIGHT      PF1
+#define SW_LEFT       PB0
+#define SW_UP         PF0
+#define SW_DOWN       PB1
+#define VIB_0         PA8
+#define VIB_1         PA11
+#define VIB_2         PA12
+
+#define SPI_MOSI      PB5
+#define SPI_MISO      PB4
+#define SPI_SCK       PB3
+#define SPI_CS_PIN    PA6
+#define SPI_INT       PA7
+
+#define I2C_SDA       PB7
+#define I2C_SCL       PB6
+
+#define DXIF_TXD      PA2
+#define DXIF_RXD      PA3
+#define DXIF_DIR      PA0
+
+MCP_CAN CAN0(SPI_CS_PIN);
+HardwareSerial dxif(DXIF_RXD, DXIF_TXD);
+Dynamixel2Arduino dxl(dxif, DXIF_DIR);
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+cap1203 cap_sens(&Wire);
+
+volatile unsigned char buff0x1b[3];
+volatile unsigned char buff0x1c[4];
+
+uint8_t vib0_count;
+uint8_t vib1_count;
+uint8_t vib2_count;
+
+float servo_angle = 0.0;
+bool servo_enable = false;
+const uint8_t DXL_ID = 1;
+const float DXL_PROTOCOL_VERSION = 2.0;
+using namespace ControlTableItem;
+
+void mcpISR(){
+  //Serial.println("isr");
+  long unsigned int id;
+  unsigned char len = 0;
+  unsigned char buff[8];
+  
+  CAN0.readMsgBuf(&id, &len, buff);
+  
+  if((id & 0x80000000) == 0x80000000){return;}
+
+  if(id == 0x1b)
+  {
+    memcpy((unsigned char *)buff0x1b, buff, 3);
+    vib0_count = (buff0x1b[0]!=0) ? buff0x1b[0] : vib0_count;
+    vib1_count = (buff0x1b[1]!=0) ? buff0x1b[1] : vib1_count;
+    vib2_count = (buff0x1b[2]!=0) ? buff0x1b[2] : vib2_count;
+  }
+  if(id == 0x1c)
+  {
+    if(servo_enable)
+    {
+      memcpy((unsigned char *)buff0x1c, buff, 4);
+      float* tmp = (float*)buff0x1c;
+      servo_angle = *tmp;
+      dxl.setGoalPosition(DXL_ID, servo_angle, UNIT_DEGREE);
+    }
+  }
+}
+void task10ms(void *pvParameters)
+{
+  portTickType xLastWakeTime;
+  const portTickType xFrequency = 10;
+  xLastWakeTime = xTaskGetTickCount();
+  while(1)
+  {
+    if(vib0_count>0){digitalWrite(VIB_0, HIGH);vib0_count--;}
+    else{digitalWrite(VIB_0, LOW);}
+    if(vib1_count>0){digitalWrite(VIB_1, HIGH);vib1_count--;}
+    else{digitalWrite(VIB_1, LOW);}
+    if(vib2_count>0){digitalWrite(VIB_2, HIGH);vib2_count--;}
+    else{digitalWrite(VIB_2, LOW);}
+
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+  }
+}
+
+
+void task20ms(void *pvParameters)
+{
+  portTickType xLastWakeTime;
+  const portTickType xFrequency = 20;
+  xLastWakeTime = xTaskGetTickCount();
+  while(1)
+  {
+    uint16_t tof;
+    byte data0x12[4] = {0};
+    tof = lox.readRange();
+    data0x12[0] = (tof & 0x00ff) >> 0;
+    data0x12[1] = (tof & 0xff00) >> 8;
+    data0x12[2] = cap_sens.getSensorInput1DeltaCountReg();
+    data0x12[3] = cap_sens.getSensorInputStatusReg();
+    cap_sens.setMainControlReg(false, false, false);
+    CAN0.sendMsgBuf(0x12, 0, 4, data0x12);
+    vTaskDelay(1);
+    
+    byte data0x13[1] = {0}; 
+    data0x13[0] = digitalRead(SW_RIGHT) << 0
+                | digitalRead(SW_LEFT)  << 1
+                | digitalRead(SW_UP)    << 2
+                | digitalRead(SW_DOWN)  << 3;
+    data0x13[0] = (~data0x13[0])&0x0f;
+    CAN0.sendMsgBuf(0x13, 0, 1, data0x13);            
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+  }
+}
+
+void setup()
+{
+  SPI.setMISO(SPI_MISO);
+  SPI.setMOSI(SPI_MOSI);
+  SPI.setSCLK(SPI_SCK);
+
+  Wire.setSDA(I2C_SDA);
+  Wire.setSCL(I2C_SCL);
+  Wire.begin();
+
+  Serial.setRx(PA10);
+  Serial.setTx(PA9);
+  Serial.begin(115200);
+  dxl.begin(57600);
+
+  pinMode(SW_RIGHT, INPUT_PULLUP);
+  pinMode(SW_LEFT, INPUT_PULLUP);
+  pinMode(SW_UP, INPUT_PULLUP);
+  pinMode(SW_DOWN, INPUT_PULLUP);
+  pinMode(VIB_0, OUTPUT);
+  pinMode(VIB_1, OUTPUT);
+  pinMode(VIB_2, OUTPUT);
+
+  vib0_count = 0;
+  vib1_count = 0;
+  vib2_count = 0;
+
+  servo_angle = 0.0;
+
+  CAN0.begin(MCP_ANY, CAN_1000KBPS, MCP_20MHZ);
+  CAN0.setMode(MCP_NORMAL);
+  pinMode(SPI_INT, INPUT);
+
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  if(dxl.ping(DXL_ID) == true)
+  {
+    servo_enable = true;
+    dxl.torqueOff(DXL_ID);
+    dxl.setOperatingMode(DXL_ID, OP_POSITION);
+    dxl.torqueOn(DXL_ID);
+    dxl.setGoalPosition(DXL_ID, 0.0, UNIT_DEGREE);
+  }
+
+  lox.begin();
+  lox.startRangeContinuous();
+
+  cap_sens.begin();
+  cap_sens.setSensitivityControlReg(BASE_DEF, DELTA_4X);
+
+  xTaskCreate(task10ms, "task10ms", configMINIMAL_STACK_SIZE, NULL, 9,  NULL);
+  xTaskCreate(task20ms, "task20ms", configMINIMAL_STACK_SIZE, NULL, 9,  NULL);
+  
+  attachInterrupt(digitalPinToInterrupt(SPI_INT), &mcpISR, FALLING);
+  
+  vTaskStartScheduler(); 
+}
+
+void loop()
+{
+  delay(1);
+}
