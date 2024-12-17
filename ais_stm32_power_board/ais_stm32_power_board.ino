fdc4a603cde5f34fe052710242432b8562a2a852
土屋 雄一 <59544467+yuichi-tsuchiya-kufusha@users.noreply.github.com>
2024-12-17 14:22:48 +0900

    add mass-production codes

--- ais_stm32_power_board/ais_stm32_power_board.ino
+++ ais_stm32_power_board/ais_stm32_power_board.ino
@@ -5,11 +5,9 @@
 
 #define SW_0          PA0
 #define SW_1          PA1
-#define SW_2          PA2
 #define SW_EW         PA12
 #define LED_0         PA3
 #define LED_1         PA4
-#define LED_2         PA5
 #define G_24V         PA6
 #define G_12V_PC      PA7
 #define G_12V_D455_1  PA11
@@ -17,6 +15,8 @@
 #define G_12V_D455_3  PB0
 #define G_5V_MCU      PA8
 #define PWM_FAN       PB1
+#define PC_ENABLE     PA2
+#define MUX_RST       PA5
 
 #define SPI_MOSI      PB5
 #define SPI_MISO      PB4
@@ -30,14 +30,36 @@
 #define STM32_TXD     PA9
 #define STM32_RXD     PA10
 
-#define ADDR_BATT     0x0b
-#define ADDR_MUX      0x70
+#define SMBUS_BATT    0x0b
+#define SMBUS_MUX     0x70
 
-#define ADDR_VOLTAGE  0x09
-#define ADDR_CULLENT  0x0a
-#define ADDR_CHARGE   0x0d 
-#define ADDR_TEMP     0x08
-#define ADDR_SERIAL   0x1c
+#define SMBUS_VOLTAGE 0x09
+#define SMBUS_CULLENT 0x0a
+#define SMBUS_CHARGE  0x0d 
+#define SMBUS_TEMP    0x08
+#define SMBUS_SERIAL  0x1c
+
+//for mass production model
+#define ADDR_EMS      0x100
+#define ADDR_STAT     0x101
+#define ADDR_IND      0x102
+#define ADDR_SEQ      0x103
+#define ADDR_ODRIVE   0x108
+#define ADDR_PC       0x109
+#define ADDR_D455_1   0x10a
+#define ADDR_D455_2   0x10b
+#define ADDR_D455_3   0x10c
+#define ADDR_MCU      0x10d
+#define ADDR_PWM      0x10e
+#define ADDR_BAT_1    0x518
+#define ADDR_BAT_2    0x519
+#define ADDR_BAT_3    0x51a
+#define ADDR_BAT_4    0x51b
+#define ADDR_BAT_SN   0x520
+#define CAN_FILTER    0x01080000
+#define CAN_MASK      0x07f80000
+#define SHUTDOWN_PC   60000   //pc shutdown wait time[ms]
+#define SHUTDOWN_FRC  120000  //force shutdown time[ms]
 
 MCP_CAN CAN0(SPI_CS_PIN);
 
@@ -45,27 +67,29 @@ volatile SemaphoreHandle_t semaphoreSequence;
 
 volatile bool flag_power_on   = false;
 volatile bool flag_shutdown   = false;
-volatile bool flag_rebooting  = false;
 volatile bool flag_emergency  = false;
 volatile bool flag_sequencing = false;
 
-volatile unsigned char buff0x15[1];
-volatile unsigned char buff0x16[1];
-volatile unsigned char buff0x17[1];
-volatile unsigned char buff0x18[1];
-volatile unsigned char buff0x19[1];
-volatile unsigned char buff0x1a[1];
-volatile unsigned char buff0x1e[1];
+volatile unsigned char buff_odrive[1];
+volatile unsigned char buff_pc[1];
+volatile unsigned char buff_d455_1[1];
+volatile unsigned char buff_d455_2[1];
+volatile unsigned char buff_d455_3[1];
+volatile unsigned char buff_mcu[1];
+volatile unsigned char buff_pwm[1];
 
 int poweron_cnt   = 0;
 int shutdown_cnt  = 0;
-int reboot_cnt    = 0;
 
+//shutdown sequence count
+int32_t sequence_cnt  = 0;
+
+//debug
 int reset_count = 0;
 
 void setChannel(uint8_t ch)
 {
-  Wire.beginTransmission(ADDR_MUX);
+  Wire.beginTransmission(SMBUS_MUX);
   switch(ch) {
       case 0:
         Wire.write(0x01);
@@ -87,10 +111,10 @@ void setChannel(uint8_t ch)
 uint16_t readWord(uint8_t addr)
 {
   uint16_t ret = 0;
-  Wire.beginTransmission(ADDR_BATT);
+  Wire.beginTransmission(SMBUS_BATT);
   Wire.write(addr);
   Wire.endTransmission(false);
-  Wire.requestFrom(ADDR_BATT, 2, true);
+  Wire.requestFrom(SMBUS_BATT, 2, true);
   ret = Wire.read();
   ret |= Wire.read()<<8;
   delayMicroseconds(5);
@@ -104,6 +128,13 @@ int checkSlave(uint8_t addr)
   err = Wire.endTransmission( );
   if(err != 0)
   {
+    digitalWrite(MUX_RST, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(MUX_RST, LOW);
+    delayMicroseconds(500);
+    digitalWrite(MUX_RST, HIGH);
+    delayMicroseconds(500);
+    
     Wire.end();
     delayMicroseconds(500);
     Wire.begin();
@@ -125,55 +156,51 @@ void mcpISR(){
   
     if((id & 0x80000000) == 0x80000000){return;}
 
-    if(id == 0x15)
+    if(id == ADDR_ODRIVE)
     {
-      memcpy((unsigned char *)buff0x15, buff, 1);
-      (buff0x15[0]&0x01 == 0x01) ? (digitalWrite(G_24V, HIGH)) : (digitalWrite(G_24V, LOW));
-      if(buff0x15[0]&0x01 == 0x01 && flag_emergency == true)
+      memcpy((unsigned char *)buff_odrive, buff, 1);
+      (buff_odrive[0]&0x01 == 0x01) ? (digitalWrite(G_24V, HIGH)) : (digitalWrite(G_24V, LOW));
+      if(buff_odrive[0]&0x01 == 0x01 && flag_emergency == true)
       {
         flag_emergency = false;
       }
     }
-    if(id == 0x16)
+    if(id == ADDR_PC)
     {
-      memcpy((unsigned char *)buff0x16, buff, 1);
-      if(buff0x16[0] == 0x00)
+      memcpy((unsigned char *)buff_pc, buff, 1);
+      if(buff_pc[0] == 0x00)
       {
         shutdownISR();
       }
-      if(buff0x16[0] == 0x01)
+      if(buff_pc[0] == 0x01)
       {
         poweronISR();
       }
-      if(buff0x16[0] == 0xff)
-      {
-        rebootISR();
-      }
     }
-    if(id == 0x17)
+    if(id == ADDR_D455_1)
     {
-      memcpy((unsigned char *)buff0x17, buff, 1);
-      (buff0x17[0]&0x01 == 0x01) ? (digitalWrite(G_12V_D455_1, HIGH)) : (digitalWrite(G_12V_D455_1, LOW));
+      memcpy((unsigned char *)buff_d455_1, buff, 1);
+      (buff_d455_1[0]&0x01 == 0x01) ? (digitalWrite(G_12V_D455_1, HIGH)) : (digitalWrite(G_12V_D455_1, LOW));
     }
-    if(id == 0x18)
+    if(id == ADDR_D455_2)
     {
-      memcpy((unsigned char *)buff0x18, buff, 1);
-      (buff0x18[0]&0x01 == 0x01) ? (digitalWrite(G_12V_D455_2, HIGH)) : (digitalWrite(G_12V_D455_2, LOW));
+      memcpy((unsigned char *)buff_d455_2, buff, 1);
+      (buff_d455_2[0]&0x01 == 0x01) ? (digitalWrite(G_12V_D455_2, HIGH)) : (digitalWrite(G_12V_D455_2, LOW));
     }
-    if(id == 0x19)
+    if(id == ADDR_D455_3)
     {
-      memcpy((unsigned char *)buff0x19, buff, 1);
-      (buff0x19[0]&0x01 == 0x01) ? (digitalWrite(G_12V_D455_3, HIGH)) : (digitalWrite(G_12V_D455_3, LOW));
+      memcpy((unsigned char *)buff_d455_3, buff, 1);
+      (buff_d455_3[0]&0x01 == 0x01) ? (digitalWrite(G_12V_D455_3, HIGH)) : (digitalWrite(G_12V_D455_3, LOW));
     }
-    if(id == 0x1a)
+    if(id == ADDR_MCU)
     {
-      memcpy((unsigned char *)buff0x1a, buff, 1);
-      (buff0x1a[0]&0x01 == 0x01) ? (digitalWrite(G_5V_MCU, HIGH)) : (digitalWrite(G_5V_MCU, LOW));
+      memcpy((unsigned char *)buff_mcu, buff, 1);
+      (buff_mcu[0]&0x01 == 0x01) ? (digitalWrite(G_5V_MCU, HIGH)) : (digitalWrite(G_5V_MCU, LOW));
     }
-    if(id == 0x1e)
+    if(id == ADDR_PWM)
     {
-      memcpy((unsigned char *)buff0x1e, buff, 1);
-      analogWrite(PWM_FAN, 255 - buff0x1e[0]);
+      memcpy((unsigned char *)buff_pwm, buff, 1);
+      analogWrite(PWM_FAN, 255 - buff_pwm[0]);
     }
   }  
 }
@@ -188,7 +215,7 @@ void emergencyISR()
     flag_emergency = true;
     byte data[1] = {0};
     data[0] = 0x01;
-    CAN0.sendMsgBuf(0x01, 0, 1, data);
+    CAN0.sendMsgBuf(ADDR_EMS, 0, 1, data);
     taskEXIT_CRITICAL();
   }
 }
@@ -223,22 +250,7 @@ void shutdownISR()
   }
 }
 
-void rebootISR()
-{
-  if(flag_sequencing == false && flag_power_on == true)
-  {
-    Serial.println("reboot");
-    flag_sequencing = true;
-    digitalWrite(LED_2, HIGH);
-    flag_rebooting  = true;
-    //byte data[1] = {0};
-    //data[0] = 0xff;
-    //CAN0.sendMsgBuf(0x02, 0, 1, data);
-    xSemaphoreGiveFromISR(semaphoreSequence, NULL);
-  }
-}
-
-//shutdown/reboot sequence task
+//poweron/shutdown sequence task
 void task_sequence(void *pvParameters)
 {
   while(1)
@@ -247,10 +259,10 @@ void task_sequence(void *pvParameters)
     Serial.println("sequence");
     byte data[1] = {0};
     
-    if(flag_power_on == true && flag_shutdown == false && flag_rebooting == false)
+    if(flag_power_on == true && flag_shutdown == false)
     {
       data[0] = 0x01;
-      CAN0.sendMsgBuf(0x02, 0, 1, data);
+      CAN0.sendMsgBuf(ADDR_SEQ, 0, 1, data);
       
       digitalWrite(G_24V, HIGH);
       vTaskDelay(100);
@@ -269,15 +281,27 @@ void task_sequence(void *pvParameters)
 
       flag_power_on  = true;
       flag_shutdown  = false;
-      flag_rebooting = false;
     }
-    else if(flag_power_on == true && flag_shutdown == true && flag_rebooting == false)
+    else if(flag_power_on == true && flag_shutdown == true)
     {
       data[0] = 0x00;
-      CAN0.sendMsgBuf(0x02, 0, 1, data);
+      CAN0.sendMsgBuf(ADDR_SEQ, 0, 1, data);
+      
+      //shutdown sequence
+      while(sequence_cnt<SHUTDOWN_FRC)
+      {
+        //pc shutdown detect
+        if(digitalRead(PC_ENABLE) == LOW)
+        {
+          vTaskDelay(5000);
+          break;
+        }
+
+        vTaskDelay(1);
+        sequence_cnt++;
+      }
       
       digitalWrite(LED_0, LOW);
-      vTaskDelay(5000);
       digitalWrite(G_24V, LOW);
       digitalWrite(G_12V_PC, LOW);
       digitalWrite(G_12V_D455_1, LOW);
@@ -286,51 +310,13 @@ void task_sequence(void *pvParameters)
       digitalWrite(G_5V_MCU, LOW);
       analogWrite(PWM_FAN, 255);
       digitalWrite(LED_1, LOW);
-    
+
+      sequence_cnt = 0;
       flag_power_on  = false;
       flag_shutdown  = false;
-      flag_rebooting = false;
       flag_emergency = false;
     }
-    else if(flag_power_on == true && flag_shutdown == false && flag_rebooting == true)
-    {
-      data[0] = 0xff;
-      CAN0.sendMsgBuf(0x02, 0, 1, data);
-      
-      digitalWrite(LED_0, LOW);
-      vTaskDelay(5000);
-      digitalWrite(G_24V, LOW);
-      digitalWrite(G_12V_PC, LOW);
-      digitalWrite(G_12V_D455_1, LOW);
-      digitalWrite(G_12V_D455_2, LOW);
-      digitalWrite(G_12V_D455_3, LOW);
-      digitalWrite(G_5V_MCU, LOW);
-      analogWrite(PWM_FAN, 255);
-
-      vTaskDelay(1000);
-
-      digitalWrite(G_24V, HIGH);
-      vTaskDelay(100);
-      digitalWrite(G_12V_PC, HIGH);
-      vTaskDelay(100);
-      digitalWrite(G_12V_D455_1, HIGH);
-      vTaskDelay(100);
-      digitalWrite(G_12V_D455_2, HIGH);
-      vTaskDelay(100);
-      digitalWrite(G_12V_D455_3, HIGH);
-      vTaskDelay(100);
-      digitalWrite(G_5V_MCU, HIGH);
-      vTaskDelay(100);
-      analogWrite(PWM_FAN, 255);
-      vTaskDelay(100);
-      digitalWrite(LED_0, HIGH);
-      digitalWrite(LED_2, LOW);
     
-      flag_power_on  = true;
-      flag_shutdown  = false;
-      flag_rebooting = false;
-      flag_emergency = false;
-    }
     flag_sequencing = false;
     vTaskDelay(1);
   }
@@ -347,10 +333,9 @@ void task_send(void *pvParameters)
     Serial.println("send");
     byte indicator[1] = {0};
     indicator[0] = digitalRead(LED_0) << 0
-                 | digitalRead(LED_1) << 1
-                 | digitalRead(LED_2) << 2;
+                 | digitalRead(LED_1) << 1;
     detachInterrupt(digitalPinToInterrupt(SPI_INT));
-    CAN0.sendMsgBuf(0x03, 0, 1, indicator);
+    CAN0.sendMsgBuf(ADDR_IND, 0, 1, indicator);
     attachInterrupt(digitalPinToInterrupt(SPI_INT), &mcpISR, FALLING);
     if(digitalRead(SPI_INT) == LOW){mcpISR();}
     vTaskDelay(1);
@@ -369,13 +354,13 @@ void task_send(void *pvParameters)
       setChannel(i);
       
       int err = 0;
-      err = checkSlave(ADDR_MUX);
+      err = checkSlave(SMBUS_MUX);
       Serial.print("mux  ");
       Serial.print(i);
       Serial.print(" ");
       Serial.println(err);
       
-      err = checkSlave(ADDR_BATT);
+      err = checkSlave(SMBUS_BATT);
       Serial.print("batt ");
       Serial.print(i);
       Serial.print(" ");
@@ -384,11 +369,11 @@ void task_send(void *pvParameters)
       Serial.print("reset ");
       Serial.println(reset_count);
 
-      voltage = readWord(ADDR_VOLTAGE); //Voltage[mV]
-      current = readWord(ADDR_CULLENT); //Current[mA]
-      charge  = readWord(ADDR_CHARGE);  //RelativeStateOfCharge[%]
-      temp    = readWord(ADDR_TEMP);    //Temperature[0.1k]
-      sn      = readWord(ADDR_SERIAL);  //SerialNumber
+      voltage = readWord(SMBUS_VOLTAGE); //Voltage[mV]
+      current = readWord(SMBUS_CULLENT); //Current[mA]
+      charge  = readWord(SMBUS_CHARGE);  //RelativeStateOfCharge[%]
+      temp    = readWord(SMBUS_TEMP);    //Temperature[0.1k]
+      sn      = readWord(SMBUS_SERIAL);  //SerialNumber
       
       battery [0] = (voltage&0x00ff)  >> 0;
       battery [1] = (voltage&0xff00)  >> 8;
@@ -403,7 +388,7 @@ void task_send(void *pvParameters)
       battery_sn[i*2+1] = (sn&0xff00) >> 8;
 
       detachInterrupt(digitalPinToInterrupt(SPI_INT));
-      CAN0.sendMsgBuf(0x05+i, 0, 8, battery);
+      CAN0.sendMsgBuf(ADDR_BAT_1+i, 0, 8, battery);
       attachInterrupt(digitalPinToInterrupt(SPI_INT), &mcpISR, FALLING);
       if(digitalRead(SPI_INT) == LOW){mcpISR();}
       vTaskDelay(1);
@@ -419,14 +404,14 @@ void task_send(void *pvParameters)
              | digitalRead(G_12V_PC)     << 4
              | digitalRead(G_24V)        << 5;
     detachInterrupt(digitalPinToInterrupt(SPI_INT));
-    CAN0.sendMsgBuf(0x14, 0, 1, data);
+    CAN0.sendMsgBuf(ADDR_STAT, 0, 1, data);
     attachInterrupt(digitalPinToInterrupt(SPI_INT), &mcpISR, FALLING);
     if(digitalRead(SPI_INT) == LOW){mcpISR();}
     vTaskDelay(1);
     //delayMicroseconds(500);
 
     detachInterrupt(digitalPinToInterrupt(SPI_INT));
-    CAN0.sendMsgBuf(0x1d, 0, 8, battery_sn);
+    CAN0.sendMsgBuf(ADDR_BAT_SN, 0, 8, battery_sn);
     attachInterrupt(digitalPinToInterrupt(SPI_INT), &mcpISR, FALLING);
     if(digitalRead(SPI_INT) == LOW){mcpISR();}
     
@@ -449,17 +434,11 @@ void setup()
   Serial.setTx(STM32_TXD);
   Serial.begin(115200);
 
-  //Serial2.setRx(PNUM_NOT_DEFINED);
-  //Serial2.setTx(PNUM_NOT_DEFINED);
-  //Serial.println("booting");
-
   pinMode(SW_0, INPUT);
   pinMode(SW_1, INPUT);
-  pinMode(SW_2, INPUT);
   pinMode(SW_EW, INPUT);
   pinMode(LED_0, OUTPUT);
   pinMode(LED_1, OUTPUT);
-  pinMode(LED_2, OUTPUT);
   pinMode(G_24V, OUTPUT);
   pinMode(G_12V_PC, OUTPUT);
   pinMode(G_12V_D455_1, OUTPUT);
@@ -467,13 +446,26 @@ void setup()
   pinMode(G_12V_D455_3, OUTPUT);
   pinMode(G_5V_MCU, OUTPUT);
   pinMode(PWM_FAN, OUTPUT);
+  pinMode(PC_ENABLE,INPUT);
+  pinMode(MUX_RST, OUTPUT);
+
+  //SMBus multiplexer reset
+  digitalWrite(MUX_RST, HIGH);
+  delay(10);
+  digitalWrite(MUX_RST, LOW);
+  delay(10);
+  digitalWrite(MUX_RST, HIGH);
+  delay(10);
+
+  digitalWrite(LED_0, LOW);
+  digitalWrite(LED_1, LOW);
 
   CAN0.begin(MCP_STDEXT, CAN_1000KBPS, MCP_20MHZ);
-  CAN0.init_Mask(0,0,0x07fc0000);
-  CAN0.init_Filt(0,0,0x00140000);
-  CAN0.init_Filt(1,0,0x00180000);
-  CAN0.init_Mask(1,0,0x07ff0000);
-  CAN0.init_Filt(2,0,0x001e0000);
+
+  //for mass production model
+  CAN0.init_Mask(0,0,CAN_MASK);
+  CAN0.init_Filt(0,0,CAN_FILTER);
+  
   CAN0.setMode(MCP_NORMAL);
   pinMode(SPI_INT, INPUT);
 
@@ -530,23 +522,6 @@ void loop()
   else if(digitalRead(SW_1) != LOW)
   {
     shutdown_cnt = 0;
-  }
-  
-  if(digitalRead(SW_2) == LOW && flag_sequencing == false && flag_power_on == true)
-  {
-    if(reboot_cnt >= 2000)
-    {
-      reboot_cnt = 0;
-      rebootISR();
-    }
-    else
-    {
-      reboot_cnt++;
-    }
-  }
-  else if(digitalRead(SW_2) != LOW)
-  {
-    reboot_cnt = 0;
   }
   
   //Serial.println(poweron_cnt);
